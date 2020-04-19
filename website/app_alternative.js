defaf121ee6693c3ff64db75b0e25ea691c076f4
SergeChurkin-ca <sergechurkin@yahoo.com>
2020-04-18 21:26:02 -0400

    posting

--- website/app_alternative.js
+++ website/app_alternative.js
@@ -9,7 +9,8 @@ const city = document.getElementById('city').value;
 const feelings = document.getElementById('feelings').value;
 
 
-document.getElementById('generate').addEventListener('click', performAction);
+document.getElementById('generate').addEventListener('click', performAction, false);
+document.getElementById('generate').addEventListener('click', addPost, false);
 
 // Event listener and rendering message on page
 function performAction() {
@@ -17,7 +18,28 @@ function performAction() {
         .then(res => res.json())
         .then(data => {
             console.log(data.name);
-            messageBody.innerHTML = `${data.name},${data.sys.country} ${Math.round(data.main.temp -273.15)}C ${data.weather[0].description}, feels like ${Math.round(data.main.feels_like -273.15)}C. <br>${feelings}`;
+            messageBody.innerHTML = `${data.name},${data.sys.country} ${Math.round(data.main.temp -273.15)}C ${data.weather[0].description}, feels like ${Math.round(data.main.feels_like -273.15)}C. <br>${feelings}<br>`;
         })
         .catch(error => console.log(error))
 }
+
+// POST
+function addPost(e) {
+    e.preventDefault();
+
+    let city = document.getElementById('city').value;
+    let feelings = document.getElementById('feelings').value;
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify({ city, feelings })
+        })
+        .then(res => res.json())
+        .then((data) => console.log(data))
+}
+
+// GET posted messages
